619b161c83d8a2be7eb5410d814ee7f6c4a4b59a
CourtneyWatts <courtney.watts@icloud.com>
2019-11-20 10:29:19 +0000

    questions and summary screen styling

--- src/screens/Summary.js
+++ src/screens/Summary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import quizDatabase from '../questionDatabase'
+import { ReactComponent as BackArrow } from '../images/icons/backArrow.svg'
 
 class Summary extends React.Component {
   constructor (props) {
@@ -12,10 +13,21 @@ class Summary extends React.Component {
   render () {
     const accolades = quizDatabase[this.props.category].accolades[this.props.results.correctTotal]
     const oneWord = accolades[0]
-    const status = accolades[1]
+    // const status = accolades[1]
     const person = accolades[2]
     const personImage = accolades[3]
-    const ranking = this.props.results.ranking
+    // const ranking = this.props.results.ranking
+    let topic1
+    let topic2
+    if (this.props.category === 'films') {
+      topic1 = topic2 = 'film'
+    } else if (this.props.category === 'music') {
+      topic1 = 'musical'
+      topic2 = 'music'
+    } else if (this.props.category === 'sports') {
+      topic1 = 'sporting'
+      topic2 = 'sports'
+    }
 
     return (
       <div>
@@ -27,22 +39,21 @@ class Summary extends React.Component {
             <span className='field'>Correct:</span><span className='value'>{this.props.results.correctTotal}</span>
           </div>
           <div className='results-box d-flex justify-content-between'>
-            <span className='field'>Streak Bonus:</span><span className='value'>{this.props.results.correctStreak}</span>
+            <span className='field'>Longest Streak:</span><span className='value'>{this.props.results.longestCorrectStreak}</span>
           </div>
           <div className='results-box d-flex justify-content-between'>
             <span className='field'>Lifes Used:</span><span className='value'>All</span>
           </div>
           <div className='mt-5 talent-like'>
-            <p className='description text-center'>If your xxx talent was a reflection of your xxx knowledge you would be...</p>
+            <p className='description text-center'>If your {topic1} talent was a reflection of your {topic2} knowledge, you would be...</p>
             <img className='reflection-image' src={personImage} />
             <div className='d-flex'><p className='reflection-name text-center'>{person}</p></div>
           </div>
-          <p>ranking: {ranking}</p>
+        </div>
+        <div className='mt-5 ml-4'>
+          <div onClick={this.props.onClick} className='back-arrow'><BackArrow /></div>
         </div>
 
-        <p>status: {status}</p>
-        <p>ranking: {ranking}</p>
-        <div onClick={this.props.onClick}> Back to Start </div>
       </div>
     )
   }
